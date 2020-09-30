eedd23fe54d27e6df1ca3d05a03734e5a8c9caf7
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-10-01 00:55:40 +0300

    feat: дополнить схему пользователя новыми полями и методом поиска

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
-const { validateText, validateUrl } = require('../helpers/validators');
+const { AuthorizedError } = require('../helpers/Errors');
+const { validateText } = require('../helpers/validators');
+const { ERROR_MESSAGE } = require('../constants');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -43,7 +46,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 8,
+    select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .orFail(new AuthorizedError(ERROR_MESSAGE.UNAUTHORIZED))
+    .then((user) => bcrypt.compare(password, user.password)
+      .then((matched) => {
+        if (!matched) {
+          throw new AuthorizedError(ERROR_MESSAGE.UNAUTHORIZED);
+        }
+        return user;
+      }));
+};
 
 module.exports = mongoose.model('user', userSchema);
